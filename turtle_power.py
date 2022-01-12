7624b457a82183d7c7382a752a3de6b332b37025
Katherine T. Chang <56325380+ka-chang@users.noreply.github.com>
2022-01-12 00:45:33 -0800

    Add files via upload

---
+++ turtle_power.py
@@ -0,0 +1,16 @@
+"""
+Cowabunga
+"""
+
+from turtle import Screen
+import time
+
+Screen().setup(width=1.0, height=1.0)
+time.sleep(1)
+Screen().bgcolor("black")
+time.sleep(1)
+Screen().bgcolor("white")
+time.sleep(1)
+Screen().bye()
+
+quit()
